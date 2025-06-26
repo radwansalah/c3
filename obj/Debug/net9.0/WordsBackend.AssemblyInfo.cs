f9bf2f5a3f37cab2355df133af6747a389c85d54
radwan Salah <radwansalah@radwans-MacBook-Pro.local>
2025-06-26 16:03:36 +0300

    Remove Folder

--- obj/Debug/net9.0/WordsBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/WordsBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordsBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5e139dbe9d01112e1d73c1371e22e416d7eea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordsBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordsBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
